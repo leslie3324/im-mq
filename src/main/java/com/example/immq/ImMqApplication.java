e4a3f69e199275dfd34dce66e249b774ccb82d22
leslie3324 <leslie_10345@126.com>
2024-01-10 17:44:32 +0800

    update:mq搭建和producer consumer测试访问

---
+++ src/main/java/com/example/immq/ImMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.immq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImMqApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/immq/producer/SyncProducer.java
@@ -0,0 +1,34 @@
+package com.example.immq.producer;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class SyncProducer {
+    public static void main(String[] args) throws Exception {
+        // 初始化一个producer并设置Producer group name
+        DefaultMQProducer producer = new DefaultMQProducer("test"); //（1）
+        producer.createTopic( null, "TopicTest",4);
+        // 设置NameServer地址
+        producer.setNamesrvAddr("172.17.212.15:9876");  //（2）
+        // 启动producer
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            // 创建一条消息，并指定topic、tag、body等信息，tag可以理解成标签，对消息进行再归类，RocketMQ可以在消费端对tag进行过滤
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );   //（3）
+            // 利用producer进行发送，并同步等待发送结果
+            SendResult sendResult = producer.send(msg);   //（4）
+            System.out.printf("%s%n", sendResult);
+        }
+        // 一旦producer不再使用，关闭producer
+        producer.shutdown();
+    }
+}
